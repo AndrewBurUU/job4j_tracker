ad5678ce3be2337c6a13b1c66b2d365aecf4fad5
AndrewBurUU <andriv80@gmail.com>
2020-07-30 19:10:26 +0800

    3. Реализация профессий в коде [#312409]

---
+++ src/main/java/ru/job4j/oop/Profession.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Profession {
+    private String name;
+    private String surname;
+    private String education;
+    private String birthday;
+
+    public String getName () {return "";}
+    public String getSurname () {return "";}
+    public String getEducation () {return "";}
+    public String getBirthday () {return "";}
+
+}
+
