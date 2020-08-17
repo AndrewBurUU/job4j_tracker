a46f3f6584258ec50e53d0bfc48b56ec0132bedd
AndrewBurUU <andriv80@gmail.com>
2020-08-17 21:34:59 +0800

    0.2. Кидаем исключение - throw new RuntimeException [#312364]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+import ru.job4j.tracker.Input;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact f = new Fact();
+        System.out.println("Factorial is " +f.calc(-2));
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Number n shouldn't be less then zero.");
+        } else {
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+        }
+        return rsl;
+    }
+}
