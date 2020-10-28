97a5a1885173bae3448923a355a862abf9b6ca64
AndrewBurUU <andriv80@gmail.com>
2020-10-28 19:57:09 +0800

    5. Подсчет функции в диапазоне. [#312161]

--- src/test/java/ru/job4j/lambda/FuncDiapTest.java
+++ src/test/java/ru/job4j/lambda/FuncDiapTest.java
@@ -5,17 +5,34 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Math.exp;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import java.util.function.Function;
-import static ru.job4j.lambda.FuncDiap.*;
 
 public class FuncDiapTest {
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
+        FuncDiap function = new FuncDiap();
         List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whenQuadFunctionThenQuadResults() {
+        FuncDiap function = new FuncDiap();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x * x + 1);
+        List<Double> expected = Arrays.asList(51D, 73D, 99D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExpFunctionThenExpResults() {
+        FuncDiap function = new FuncDiap();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
+        assertThat(result, is(expected));
+    }
+
 }
